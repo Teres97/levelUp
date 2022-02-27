f4c37edcd44bb9347df60d63f5b1166c6d7e5864
Teres97 <teres97@mail.ru>
2022-02-27 16:06:28 +0200

    levelUp

---
+++ app/js/swiper.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper', {
+
+    direction: 'horizontal',
+    loop: true,
+    
+    autoplay: {
+      delay: 5000,
+    },
+
+    pagination: {
+      el: '.swiper-pagination',
+    }
+  });
